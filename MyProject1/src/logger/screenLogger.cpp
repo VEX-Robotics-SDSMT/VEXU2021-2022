40e12f2dcbcf254a623af3e3a71acb867799476d
RhoneG <90288788+RhoneG@users.noreply.github.com>
2021-10-02 10:26:03 -0600

    Started Implementing interface for screen logger

---
+++ MyProject1/src/logger/screenLogger.cpp
@@ -0,0 +1,29 @@
+#include "screenLogger.h"
+
+
+ScreenLogger::ScreenLogger()
+{
+  
+}
+
+ScreenLogger::ScreenLogger(brain &b)
+{
+  robotBrain = b;
+}
+
+void ScreenLogger::ClearAll()
+{
+
+}
+    
+void ScreenLogger::ClearLine(int line)
+{
+
+}
+    
+    
+void ScreenLogger::WriteLine(int line, std::string text)
+{
+
+}
+
---
+++ MyProject1/src/logger/screenLogger.h
@@ -0,0 +1,21 @@
+#ifndef _SCREENLOGGER_H
+#define _SCREENLOGGER_H
+
+#include "vex.h"
+
+class ScreenLogger
+{
+  brain *robotBrain;
+
+  public:
+    ScreenLogger();
+    ScreenLogger(brain &b);
+    void ClearAll();
+    void ClearLine(int line);
+    void WriteLine(int line, std::string text);
+
+  private:
+
+};
+
+#endif
