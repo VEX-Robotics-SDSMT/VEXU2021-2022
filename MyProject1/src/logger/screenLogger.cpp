5a4cc0a5e494b558dd8b28af78930570c5a4e3a3
dakmathdx <dakmathdx@gmail.com>
2021-10-02 12:46:27 -0600

    Update screenLogger.cpp

--- MyProject1/src/logger/screenLogger.cpp
+++ MyProject1/src/logger/screenLogger.cpp
@@ -3,7 +3,8 @@
 
 ScreenLogger::ScreenLogger()
 {
-  
+  brain *bra = &Brain;
+  robotBrain = bra;
 }
 
 ScreenLogger::ScreenLogger(brain &b)
@@ -14,17 +15,19 @@ ScreenLogger::ScreenLogger(brain &b)
 
 void ScreenLogger::ClearAll()
 {
-
+  robotBrain->Screen.clearScreen();
 }
     
 void ScreenLogger::ClearLine(int line)
 {
-
+  robotBrain->Screen.clearLine(line);
 }
     
     
 void ScreenLogger::WriteLine(int line, std::string text)
 {
-
+  robotBrain->Screen.setCursor(line,0);
+  std::string theText = text;
+  robotBrain->Screen.print(theText);
 }
 
